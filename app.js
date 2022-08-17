1b921700a8e9f50763059933182f44bb8b6ca60c
jonathas.marques102030 <jonathascraft102030gtav@gmail.com>
2022-08-16 23:47:27 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const moment = require("moment")
+const acessorios = require("./dados")
+
+
+console.log("-------Lojinha do Jonathas--------")
+console.log(acessorios)
---
+++ dados.js
@@ -0,0 +1,4 @@
+exports.acessorios = [{id: 1, nome: "Computador", valor: "15"},
+{id: 2, nome: "Tablet", valor: "30"},
+{id: 3, nome: "Notebook", valor: "60"}
+]
